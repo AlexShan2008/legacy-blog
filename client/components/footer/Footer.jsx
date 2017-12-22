be55f4f7986b52f312dd60d075e2124119705354
AlexShan <happyshantong@163.com>
2017-12-22 10:16:44 +0800

    Mis: Add new footer

--- client/components/footer/Footer.jsx
+++ client/components/footer/Footer.jsx
@@ -3,16 +3,22 @@ import React, {Component} from 'react';
 
 function ICP() {
     return (
-        <div className="ICP">
+        <div className="ICP dis_i">
             京ICP备 17040738号-1
         </div>
     )
 }
 
+function Icon() {
+    return (
+        <img className="icon dis_i" src="/img/logo-icon-gray-round.png" alt="数据可视化-AlexShan"/>
+    )
+}
+
 function Copy() {
     return (
-        <div className="copy">
-            Copyright&nbsp;&copy;&nbsp;2018 Shan&Guo
+        <div className="copy dis_i">
+            &copy;&nbsp;2018 Shan&Guo
         </div>
     )
 }
@@ -23,6 +29,7 @@ class Footer extends Component {
             <footer className="footer">
                 <div className="content">
                     <Copy/>
+                    <Icon/>
                     <ICP/>
                 </div>
             </footer>
